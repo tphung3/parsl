tphung3/parsl

commit 38614d6706c50c76985994b97010552fcb6ef2a0

Fixes #1857 by setting remote func.__name__ manually (#1859)
Manually set func.__name__ to fix Parsl stdout log filename issue when parsl.AUTO_LOGNAME is set in bash_apps
Added a test case to validate the stdout format when parsl.AUTO_LOGNAME is set

Fixes #1857

parsl/app/bash.py
@@ -129,6 +129,7 @@ class BashApp(AppBase):
         # this is done to avoid passing a function type in the args which parsl.serializer
         # doesn't support
         remote_fn = partial(update_wrapper(remote_side_bash_executor, self.func), self.func)
+        remote_fn.__name__ = self.func.__name__
         self.wrapped_remote_function = wrap_error(remote_fn)
 
     def __call__(self, *args, **kwargs):

parsl/tests/configs/local_threads.py
@@ -1,9 +1,13 @@
 from parsl.config import Config
 from parsl.executors.threads import ThreadPoolExecutor
+from parsl.tests.utils import get_rundir
 
 
 def fresh_config():
-    return Config(executors=[ThreadPoolExecutor()])
+    return Config(
+        executors=[ThreadPoolExecutor()],
+        run_dir=get_rundir(),
+    )
 
 
 config = fresh_config()

parsl/tests/test_bash_apps/test_basic.py
@@ -3,6 +3,8 @@ import os
 import pytest
 import shutil
 import time
+import random
+import re
 
 import parsl
 from parsl import File
@@ -21,7 +23,7 @@ def echo_to_file(inputs=[], outputs=[], stderr='std.err', stdout='std.out'):
 
 
 @bash_app
-def foo(x, y, z=10, stdout=None):
+def foo(x, y, z=10, stdout=None, label=None):
     return """echo {0} {1} {z}
     """.format(x, y, z=z)
 
@@ -31,23 +33,20 @@ def test_command_format_1():
     """Testing command format for BashApps
     """
 
-    stdout = os.path.abspath('std.out')
+    outdir = os.path.abspath('outputs')
+    stdout = os.path.join(outdir, 'foo-std.out')
     if os.path.exists(stdout):
         os.remove(stdout)
 
-    app_fu = foo(1, 4, stdout=stdout)
-    print("App_fu : ", app_fu)
+    foo_future = foo(1, 4, stdout=stdout)
+    print("[test_command_format_1] foo_future: ", foo_future)
     contents = None
 
-    assert app_fu.result() == 0, "BashApp exited with an error code : {0}".format(
-        app_fu.result())
+    assert foo_future.result() == 0, "BashApp exited with an error code : {0}".format(
+        foo_future.result())
 
     with open(stdout, 'r') as stdout_f:
         contents = stdout_f.read()
-        print("Contents : ", contents)
-
-    if os.path.exists('stdout_file'):
-        os.remove(stdout)
 
     assert contents == '1 4 10\n', 'Output does not match expected string "1 4 10", Got: "{0}"'.format(
         contents)
@@ -55,10 +54,37 @@ def test_command_format_1():
 
 
 @pytest.mark.issue363
+def test_auto_log_filename_format():
+    """Testing auto log filename format for BashApps
+    """
+    app_label = "label_test_auto_log_filename_format"
+    rand_int = random.randint(1000, 1000000000)
+
+    foo_future = foo(1, rand_int, stdout=parsl.AUTO_LOGNAME, label=app_label)
+    print("[test_auto_log_filename_format] foo_future: ", foo_future)
+    contents = None
+
+    assert foo_future.result() == 0, "BashApp exited with an error code : {0}".format(
+        foo_future.result())
+
+    log_fpath = foo_future.stdout
+    log_pattern = fr".*/task_\d+_foo_{app_label}"
+    assert re.match(log_pattern, log_fpath), 'Output file "{0}" does not match pattern "{1}"'.format(
+        log_fpath, log_pattern)
+    assert os.path.exists(log_fpath), 'Output file does not exist "{0}"'.format(log_fpath)
+    with open(log_fpath, 'r') as stdout_f:
+        contents = stdout_f.read()
+
+    assert contents == '1 {0} 10\n'.format(rand_int), \
+        'Output does not match expected string "1 {0} 10", Got: "{1}"'.format(rand_int, contents)
+    return True
+
+
+@pytest.mark.issue363
 def test_parallel_for(n=3):
     """Testing a simple parallel for loop
     """
-    outdir = os.path.abspath('outputs')
+    outdir = os.path.join(os.path.abspath('outputs'), 'test_parallel')
     if not os.path.exists(outdir):
         os.makedirs(outdir)
     else:
@@ -105,4 +131,5 @@ if __name__ == '__main__':
 
     x = test_parallel_for(int(args.count))
     y = test_command_format_1()
+    z = test_auto_log_filename_format()
     # raise_error(0)

commit d7e0d6af30581ba6b0e18360c7ba8ceaa43928f3

ignore type error introduced in #1859

parsl/app/bash.py
@@ -142,7 +142,13 @@ class BashApp(AppBase):
         # this is done to avoid passing a function type in the args which parsl.serializer
         # doesn't support
         remote_fn = partial(update_wrapper(remote_side_bash_executor, self.func), self.func)
-        remote_fn.__name__ = self.func.__name__
+
+        # parsl/app/bash.py:145: error: "partial[Any]" has no attribute "__name__"
+        # but... other parts of the code are relying on getting the __name__
+        # of (?) an arbitrary Callable too (which is why we're setting the __name__
+        # at all)
+        remote_fn.__name__ = self.func.__name__  # type: ignore
+
         self.wrapped_remote_function = wrap_error(remote_fn)
 
     def __call__(self, *args, **kwargs):