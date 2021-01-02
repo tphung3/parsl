dbe878dfb5679f05c816332ac02f2a7c43d9130b
Ben Clifford <benc@hawaga.org.uk>
2021-01-02 14:34:53 +0000

    fix docstring typo

--- parsl/dataflow/dflow.py
+++ parsl/dataflow/dflow.py
@@ -908,7 +908,7 @@ class DataFlowKernel(object):
         Parameters
         ----------
         provider: Provider obj
-           Provider for which scritps dirs are being created
+           Provider for which scripts dirs are being created
         channel: Channel obk
            Channel over which the remote dirs are to be created
         """
