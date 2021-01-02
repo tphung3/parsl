tphung3/parsl

commit 3e9f6051eab3ff988e4ca401752f3846f83af7fa

Use typeguard for more focused checkpoint loading typechecking

parsl/dataflow/dflow.py
@@ -1163,6 +1163,7 @@ class DataFlowKernel(object):
                                                                                   len(memo_lookup_table.keys())))
         return memo_lookup_table
 
+    @typeguard.typechecked
     def load_checkpoints(self, checkpointDirs: Optional[List[str]]) -> 'Dict[str, Future[Any]]':
         """Load checkpoints from the checkpoint files into a dictionary.
 
@@ -1177,13 +1178,10 @@ class DataFlowKernel(object):
         """
         self.memo_lookup_table = None
 
-        if not checkpointDirs:
+        if checkpointDirs:
+            return self._load_checkpoints(checkpointDirs)
+        else:
             return {}
-
-        if type(checkpointDirs) is not list:
-            raise BadCheckpoint("checkpointDirs expects a list of checkpoints")
-
-        return self._load_checkpoints(checkpointDirs)
 
     @staticmethod
     def _log_std_streams(task_record: TaskRecord) -> None:

commit 7dcb165849b2e0492fecffaca203253fc881757c

adding typeguard in 3e9f6051eab3ff988e4ca401752f3846f83af7fa broke runtime: typeguard can't deal with 'Future[Any]'

parsl/dataflow/dflow.py
@@ -1170,7 +1170,11 @@ class DataFlowKernel(object):
         return memo_lookup_table
 
     @typeguard.typechecked
-    def load_checkpoints(self, checkpointDirs: Optional[Sequence[str]]) -> 'Dict[str, Future[Any]]':
+    def load_checkpoints(self, checkpointDirs: Optional[Sequence[str]]) -> 'Dict[str, Future]':
+        # typeguard 2.10.1 cannot cope with Future[Any], giving a type-not-subscriptable error.
+        # Future with no subscript is probably equivalent though? I wanted the Any in there as
+        # an explicit note that I had the possibility of tighter typing but wasn't using it.
+        # def load_checkpoints(self, checkpointDirs: Optional[List[str]]) -> 'Dict[str, Future[Any]]':
         """Load checkpoints from the checkpoint files into a dictionary.
 
         The results are used to pre-populate the memoizer's lookup_table