62681d4ed52aec70f9e4a8232bca42f9c31d08ee
Ben Clifford <benc@hawaga.org.uk>
2021-01-13 11:55:32 +0000

    Remove incorrectly added whitespace

--- parsl/addresses.py
+++ parsl/addresses.py
@@ -81,7 +81,6 @@ def address_by_interface(ifname: str) -> str:
 
     """
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
     return socket.inet_ntoa(fcntl.ioctl(
         s.fileno(),
         0x8915,  # SIOCGIFADDR
